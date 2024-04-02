f835a9c9db1d3fb6eed1ffb38e28059ecabda17e
Your Name <lokeshtiwari212@gmail.com>
2024-04-02 14:58:49 +0530

    conditons given

--- backend/controller/reservation.js
+++ backend/controller/reservation.js
@@ -1,6 +1,10 @@
 import ErrorHandler from '../error/error.js'
 import {} from '../models/reservationSchema.js';
 
-export const sendReservation = async(req,res,next){
-    
+export const sendReservation = async(req,res,next)=>{
+const {firstName, lastname,email,phone,date,time} = req.body;
+if(firstName || lastname || email || phone || date || time ){
+    return next(new ErrorHandler("Please fill full reservation form!",400));
+}
+
 }
