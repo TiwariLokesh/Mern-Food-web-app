7c5b35297884a55d2473e8056dccc264ee90b070
Your Name <lokeshtiwari212@gmail.com>
2024-04-02 15:02:11 +0530

    implemented try & catch

--- backend/controller/reservation.js
+++ backend/controller/reservation.js
@@ -1,10 +1,19 @@
 import ErrorHandler from '../error/error.js'
-import {} from '../models/reservationSchema.js';
+import { Reservation } from '../models/reservationSchema.js';
 
 export const sendReservation = async(req,res,next)=>{
 const {firstName, lastname,email,phone,date,time} = req.body;
 if(firstName || lastname || email || phone || date || time ){
     return next(new ErrorHandler("Please fill full reservation form!",400));
 }
+try{
+    await Reservation.create(firstName, lastname,email,phone,date,time);
+    res.status(200),
+    json({
+        success: true,
+        message: "Reservation Sent Successfully!",
+    });
+}catch (error){
 
 }
+};
